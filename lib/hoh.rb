762b2195c2d91db2cd415b1f5a0a2b1698955937
Steven Kandow <kando1sr@gmail.com>
2020-03-04 15:00:23 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,13 +32,14 @@
 
 def naming_system
   identity = {
-    "Kingdom" => {
-      "Phylum" => {
-        "Class" => {
+    label: "Kingdom", sub_category => {
+      label: "Phylum", sub_category => {
+        label: "Class", sub_category => {
           "Order"
         }
       }
     }
+  }
   # Remember:
   #  Kingdom
   #  Phylum
