9f06b8fa4dbcb205e0160d24fbd05b630019661e
Steven Kandow <kando1sr@gmail.com>
2020-03-04 15:02:34 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -35,7 +35,11 @@ def naming_system
     label: "Kingdom", sub_category: {
       label: "Phylum", sub_category: {
         label: "Class", sub_category: {
-          "Order"
+          label: "Order", sub_category: {
+            label: "Genus", sub_category: {
+              label: "Species" 
+            }
+          }
         }
       }
     }
